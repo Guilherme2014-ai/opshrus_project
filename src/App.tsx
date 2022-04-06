720648b9deb6399d619cc8d1e114e1f175089d20
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-05 22:37:01 -0300

    check

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+// Components
+import { NavComponent } from "./components/NavComponent";
+
 // CSS
 import "./App.scss";
+import { DashboardComponent } from "./components/DashboardComponent";
 
 function App() {
   return (
     <Router>
+      <NavComponent />
       <Routes>
-        <Route path="/dashboard" element={<h1>Dashboard</h1>}></Route>
+        <Route path="/dashboard" element={<DashboardComponent />}></Route>
       </Routes>
     </Router>
   );
---
+++ src/components/LeftSideBarComponent.tsx
@@ -0,0 +1,15 @@
+// Dependencies
+import React from "react";
+
+// CSS
+import "./scss/LeftSideBarComponent.scss";
+
+const LeftSideBarComponent = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export { LeftSideBarComponent };
