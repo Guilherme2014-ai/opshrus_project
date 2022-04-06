9cf9830b20290ac7f2aa7b89fc46072ec5680c82
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-06 08:26:45 -0300

    Dashboard...

--- src/components/DashboardComponent.tsx
+++ src/components/DashboardComponent.tsx
@@ -3,68 +3,81 @@ import React from "react";
 
 // Components
 import { LeftSideBarComponent } from "./LeftSideBarComponent";
+import { SecondNavComponent } from "./SecondNavComponent";
 
 // Defaults
 import iconsPath from "../defaults/iconsPath";
 
 // CSS
 import "./style/DashboardComponent.scss";
-import { SecondNavComponent } from "./SecondNavComponent";
 
 const DashboardComponent = () => {
   return (
     <section className="main_dashboard_section">
       <SecondNavComponent />
-      <LeftSideBarComponent
-        divisors={[
-          {
-            title: "Menu",
-            sideBarsIcons: [
-              {
-                title: "home",
-                svgLink: iconsPath.home,
-              },
-              {
-                title: "Search",
-                svgLink: iconsPath.search,
-              },
-              {
-                title: "Browse",
-                svgLink: iconsPath.browse,
-              },
-              {
-                title: "Featured",
-                svgLink: iconsPath.start,
-              },
-              {
-                title: "Featured",
-                svgLink: iconsPath.start,
-              },
-            ],
-          },
-          {
-            title: "Creator Menu",
-            sideBarsIcons: [
-              {
-                title: "Profile",
-                svgLink: iconsPath.user,
-              },
-              {
-                title: "Projects",
-                svgLink: iconsPath.boxes,
-              },
-              {
-                title: "Clients",
-                svgLink: iconsPath.users,
-              },
-              {
-                title: "Featured",
-                svgLink: iconsPath.start,
-              },
-            ],
-          },
-        ]}
-      />
+
+      <div className="content_area">
+        {" "}
+        <LeftSideBarComponent
+          divisors={[
+            {
+              title: "Menu",
+              sideBarsIcons: [
+                {
+                  title: "home",
+                  svgLink: iconsPath.home,
+                },
+                {
+                  title: "Search",
+                  svgLink: iconsPath.search,
+                },
+                {
+                  title: "Browse",
+                  svgLink: iconsPath.browse,
+                },
+                {
+                  title: "Featured",
+                  svgLink: iconsPath.start,
+                },
+                {
+                  title: "Featured",
+                  svgLink: iconsPath.start,
+                },
+              ],
+            },
+            {
+              title: "Creator Menu",
+              sideBarsIcons: [
+                {
+                  title: "Profile",
+                  svgLink: iconsPath.user,
+                },
+                {
+                  title: "Projects",
+                  svgLink: iconsPath.boxes,
+                },
+                {
+                  title: "Clients",
+                  svgLink: iconsPath.users,
+                },
+                {
+                  title: "Featured",
+                  svgLink: iconsPath.start,
+                },
+              ],
+            },
+          ]}
+        />
+        <div className="projects_dashboard">
+          <h1>Dashboard</h1>
+          <ul>
+            <li>Ongoing Projects</li>
+            <li>Complete Projects</li>
+            <li>Messages</li>
+          </ul>
+          <div></div>
+        </div>
+      </div>
     </section>
   );
 };
