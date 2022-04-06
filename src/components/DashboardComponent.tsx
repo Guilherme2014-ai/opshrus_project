175e683b2bcc2bc98704d36a6a2ef6284e2064f7
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-05 22:50:49 -0300

    check

---
+++ src/components/DashboardComponent.tsx
@@ -0,0 +1,18 @@
+// Dependencies
+import React from "react";
+
+// Components
+import { LeftSideBarComponent } from "./LeftSideBarComponent";
+
+// CSS
+import "./scss/DashboardComponent.scss";
+
+const DashboardComponent = () => {
+  return (
+    <section>
+      <LeftSideBarComponent />
+    </section>
+  );
+};
+
+export { DashboardComponent };
---
+++ src/components/SecondNavComponent.tsx
@@ -0,0 +1,16 @@
+// Dependencies
+import React from "react";
+
+// CSS
+import "./style/SecondNavComponent.scss";
+
+const SecondNavComponent = () => {
+  return (
+    <nav className="main_nav">
+      <h2>Contenta</h2>
+      <div>USERNAME --- Avatar(MUI)</div>
+    </nav>
+  );
+};
+
+export { NavComponent };
