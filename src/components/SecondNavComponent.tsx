8adca58e37ea9229301b948c242f42c7c0873983
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-06 07:48:09 -0300

    MUI Added

--- src/components/SecondNavComponent.tsx
+++ src/components/SecondNavComponent.tsx
@@ -1,14 +1,25 @@
 // Dependencies
 import React from "react";
 
+// Components
+import { Avatar } from "@mui/material";
+
+// Defaults
+import users from "../mockedData/users";
+
 // CSS
 import "./style/SecondNavComponent.scss";
 
 const SecondNavComponent = () => {
+  const user = users.getUserByName("Thiago");
+
   return (
     <nav className="main_nav">
       <h2>Contenta</h2>
-      <div>USERNAME --- Avatar(MUI)</div>
+      <div>
+        <small>{user?.name.toLocaleUpperCase()}</small>{" "}
+        <Avatar alt="Remy Sharp" src={user?.profile_picture} />
+      </div>
     </nav>
   );
 };
