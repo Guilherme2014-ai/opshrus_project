dcf08ee4f27dff7d19501f7162f906fb40306a98
Navarropy <Adriano_navarro@outlook.com>
2022-04-07 16:31:46 -0300

    /

--- src/components/AboutUsComponent.jsx
+++ src/components/AboutUsComponent.jsx
@@ -3,18 +3,19 @@ import React from "react";
 
 // Components
 import Nav from "./Nav";
+import Circle from "./minor/Circle";
 
 // CSS
 import "./style/AboutUsComponent.scss";
 
 const AboutUsComponent = () => {
   return (
-    <section className="aboutUsMain">
+    <section className="aboutUs">
       <Nav />
 
-      <div className="aboutUsMain__mainArea">
-        <div className="aboutUsMain__mainArea__mainText">
-          <div className="aboutUsMain__mainArea__mainText__container">
+      <div className="aboutUs__mainArea">
+        <div className="aboutUs__mainArea__mainText">
+          <div className="aboutUs__mainArea__mainText__container">
             <h2>
               Build Your Business From the Ground Up With the World’s Best
               Talents
@@ -25,10 +26,46 @@ const AboutUsComponent = () => {
               Get your business up and running or start a fulfilling career with
               us now.
             </p>
-            <div className="aboutUsMain__mainArea__mainText__container__contact">
+            <div className="aboutUs__mainArea__mainText__container__contact">
               <p>Join our Network</p>
               <p>Work with us</p>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="aboutUs__howWeDoIt">
+        <div className="aboutUs__howWeDoIt__container">
+          <h2>How we do it</h2>
+          <div className="aboutUs__howWeDoIt__container__contentWrapper">
+            <Circle
+              gradient="linear-gradient(210.31deg, #FF6108 -11.63%, #FFE608 105.87%)"
+              width="92.99px"
+              height="92.99px"
+              top="0"
+              right="0"
+              bottom="0"
+              left="2%"
+            />
+            <Circle
+              gradient="linear-gradient(210.31deg, #715AFF -11.63%, #E682FF 105.87%)"
+              width="214.86px"
+              height="214.86px"
+              top="55%"
+              right="0"
+              bottom="0"
+              left="-80px"
+            />
+
+            <Circle
+              gradient="linear-gradient(210.31deg, #FF6108 -11.63%, #FFE608 105.87%)"
+              width="308.7px"
+              height="308.7px"
+              top="20%"
+              right="0"
+              bottom="0"
+              left="80%"
+            />
+            <div className="aboutUs__howWeDoIt__container__contentWrapper__content"></div>
           </div>
         </div>
       </div>
---
+++ src/components/minor/Circle.jsx
@@ -0,0 +1,18 @@
+import "./style/Circle.scss";
+
+export default function Circle(props) {
+  return (
+    <div
+      className="circle"
+      style={{
+        background: props.gradient,
+        width: props.width,
+        height: props.height,
+        top: props.top,
+        right: props.right,
+        bottom: props.bottom,
+        left: props.left,
+      }}
+    ></div>
+  );
+}
