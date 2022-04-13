a6ffe65326067d2d32ebff11b6304dd3676ee05d
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-13 16:00:57 -0300

    changes

---
+++ src/defaults/month_position.ts
@@ -0,0 +1,14 @@
+export default {
+  january: "1",
+  february: "2",
+  march: "3",
+  april: "4",
+  may: "5",
+  june: "6",
+  july: "7",
+  august: "8",
+  september: "9",
+  octuber: "10",
+  november: "11",
+  december: "12",
+};
---
+++ src/defaults/position_month.ts
@@ -0,0 +1,14 @@
+export default {
+  "1": "january",
+  "2": "february",
+  "3": "march",
+  "4": "april",
+  "5": "may",
+  "6": "june",
+  "7": "july",
+  "8": "august",
+  "9": "september",
+  "10": "octuber",
+  "11": "november",
+  "12": "december",
+};
