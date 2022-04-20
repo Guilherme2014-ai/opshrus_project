91e29763cea351a3e072ef8dd05ee3a506eda55d
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-20 18:06:48 -0300

    editProfilePage added

--- src/components/EditProfileComponent.tsx
+++ src/components/EditProfileComponent.tsx
@@ -1,88 +1,120 @@
 // Dependencies
-import { Avatar } from "@mui/material";
 import React from "react";
+
+// Components
+import { Avatar } from "@mui/material";
+
+// Interfaces
+interface input {
+  label: string;
+  type: string;
+  slug: string;
+  placeholder?: string;
+}
+
+// Contexts
+import { getUserSessionState } from "../contexts/userSessionStateContext";
+import getSessionUserProvider from "../modules/getSessionUserProvider";
+
+// Defaults
 import profilePicture from "../defaults/profilePicture";
 
 // CSS
 import "./style/EditProfileComponent.scss";
+import iconsPath from "../defaults/iconsPath";
 
 const EditProfileComponent = () => {
+  const [stringfiedUser, setStringfiedUser] = getUserSessionState();
+  const userSession = getSessionUserProvider(stringfiedUser);
+
+  const inputs: input[] = [
+    {
+      label: "USERNAME",
+      slug: "username",
+      type: "text",
+      placeholder: userSession?.name,
+    },
+    {
+      label: "PASSWORD",
+      slug: "password",
+      type: "password",
+      placeholder: "*******",
+    },
+    {
+      label: "E - MAIL",
+      slug: "email",
+      type: "email",
+      placeholder: userSession?.email,
+    },
+    {
+      label: "ABOUT ME",
+      slug: "about_me",
+      type: "text",
+      placeholder: "",
+    },
+    {
+      label: "MY AREAS OF EXPERTIES",
+      slug: "my_areas_of_experties",
+      type: "text",
+      placeholder: "*******",
+    },
+  ];
+
   return (
-    <section className="editProfileContainer">
-      <div className="editProfileContainer--firstArea subArea">
-        <form className="editProfileContainer--firstArea__form">
-          <div className="editProfileContainer--firstArea__form__inputArea">
-            <label htmlFor="username">USERNAME</label>
-            <br />
-            <input
-              type="text"
-              name="username"
-              id="username"
-              placeholder="user000"
-              readOnly
+    <section className="mainSectionEditProfilePicture">
+      <div className="editProfileContainer">
+        <div className="editProfileContainer--firstArea subArea">
+          <form className="editProfileContainer--firstArea__form">
+            {inputs.map((input: input) => {
+              return (
+                <div className="editProfileContainer--firstArea__form__inputArea">
+                  <label htmlFor="username">{input.label}</label>
+                  <br />
+                  <div className="editProfileContainer--firstArea__form__inputArea__input_svg">
+                    <input
+                      type={input.type}
+                      name={input.slug}
+                      id={input.slug}
+                      placeholder={input.placeholder}
+                      readOnly
+                    />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="white"
+                      className="bi bi-pencil"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d={iconsPath.pencil} />
+                    </svg>
+                  </div>
+                </div>
+              );
+            })}
+          </form>
+        </div>
+        <div className="editProfileContainer--secondArea subArea">
+          <div className="editProfileContainer--secondArea__avatarInfos">
+            <Avatar
+              className="editProfileContainer--secondArea__avatarInfos__avatar"
+              alt="profile_picture"
+              src={profilePicture}
             />
+            <div className="editProfileContainer--secondArea__avatarInfos__selectAvartarAreaButton">
+              <button>
+                <strong>Select Avatar</strong>
+              </button>
+            </div>
           </div>
-
-          <div className="editProfileContainer--firstArea__form__inputArea">
-            <label htmlFor="password">PASSWORD</label>
-            <br />
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="*****"
-            />
-          </div>
-
-          <div className="editProfileContainer--firstArea__form__inputArea">
-            <label htmlFor="email">E-MAIL</label>
-            <br />
-            <input
-              type="email"
-              name="email"
-              id="email"
-              placeholder="name@email.com"
-            />
-          </div>
-
-          <div className="editProfileContainer--firstArea__form__inputArea">
-            <label htmlFor="about_me">ABOUT ME</label>
-            <br />
-            <input type="text" name="about_me" id="about_me" />
-          </div>
-
-          <div className="editProfileContainer--firstArea__form__inputArea">
-            <label htmlFor="experties_areas">MY AREAS OF EXPERTIES</label>
-            <br />
-            <input
-              type="text"
-              name="experties_areas"
-              id="experties_areas"
-              placeholder="*****"
-            />
-          </div>
-        </form>
-      </div>
-      <div className="editProfileContainer--secondArea subArea">
-        <div className="editProfileContainer--secondArea__avatarInfos">
-          <Avatar
-            className="editProfileContainer--secondArea__avatarInfos__avatar"
-            alt="profile_picture"
-            src={profilePicture}
-          />
-          <div className="editProfileContainer--secondArea__avatarInfos__selectAvartarAreaButton">
-            <button>
-              <strong>Select Avatar</strong>
+          <div className="editProfileContainer--secondArea__confirm_CancelButtonsArea">
+            <button className="editProfileContainer--secondArea__confirm_CancelButtonsArea--confirm">
+              <strong>CONFIRM</strong>
+            </button>
+            <button className="editProfileContainer--secondArea__confirm_CancelButtonsArea--cancel">
+              <strong>CANCEL</strong>
             </button>
           </div>
-        </div>
-        <div className="editProfileContainer--secondArea__confirm_CancelButtonsArea">
-          <button className="editProfileContainer--secondArea__confirm_CancelButtonsArea--confirm">
-            <strong>CONFIRM</strong>
-          </button>
-          <button className="editProfileContainer--secondArea__confirm_CancelButtonsArea--cancel">
-            <strong>CANCEL</strong>
-          </button>
         </div>
       </div>
     </section>
--- src/interfaces/IUserSession.ts
+++ src/interfaces/IUserSession.ts
@@ -4,6 +4,7 @@ export default interface IUserSession {
   id: string;
   name: string;
   age: number;
+  email: string;
   profile_picture: string;
   projects: IProject[];
 }
