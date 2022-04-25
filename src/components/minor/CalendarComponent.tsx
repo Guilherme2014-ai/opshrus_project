fc3f39fadda214654541192391f529a8a2325a9f
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-25 10:49:30 -0300

    improvements

--- src/components/minor/CalendarComponent.tsx
+++ src/components/minor/CalendarComponent.tsx
@@ -5,6 +5,7 @@ import { idUniqueV2 } from "id-unique-protocol";
 
 // Defaults
 import month_position from "../../defaults/month_position";
+import iconsPath from "../../defaults/iconsPath";
 import colors from "../../defaults/colors";
 
 // Components
@@ -30,6 +31,7 @@ const CalendarComponent = ({
   const [createScheduleDay, setCreateScheduleDay] = useState(
     null,
   ) as unknown as [null | number, Dispatch<null | number>];
+  const jumpUnity = 100;
 
   useEffect(() => {
     const monthsWrapper__monthsElement = document.getElementById(
@@ -47,7 +49,7 @@ const CalendarComponent = ({
 
     function jumpWithTheWheel(e: WheelEvent) {
       const deltaY = e.deltaY;
-      const jump = deltaY < 0 ? 100 : -100;
+      const jump = deltaY < 0 ? jumpUnity : -jumpUnity;
 
       monthsWrapper__monthsElement.scrollBy(jump, 0);
     }
@@ -57,6 +59,16 @@ const CalendarComponent = ({
 
   function daysChangerHandler(monthName: TMonths) {
     setCurrentMonth(monthName);
+  }
+
+  function jumpWithTheButton(button: "left" | "right") {
+    const monthsWrapper__monthsElement = document.getElementById(
+      "monthsWrapper__months",
+    ) as HTMLElement;
+
+    const jump = button == "left" ? -jumpUnity : jumpUnity;
+
+    monthsWrapper__monthsElement.scrollBy(jump, 0);
   }
 
   const simulatedFor = (num: number): number[] => {
@@ -73,6 +85,26 @@ const CalendarComponent = ({
     <div className="calendar">
       <div className="monthsArea">
         <div className="monthsWrapper">
+          <div
+            style={{
+              position: "absolute",
+              color: "red",
+              transform: "translateX(-50px) translateY(-5px)",
+            }}
+            className="monthsWrapper__buttonWrapper"
+            onClick={() => jumpWithTheButton("left")}
+          >
+            <svg
+              key={idUniqueV2()}
+              xmlns="http://www.w3.org/2000/svg"
+              width="35"
+              height="36"
+              fill="white"
+              viewBox="0 0 16 16"
+            >
+              <path d={iconsPath.arrowLeft} />
+            </svg>
+          </div>
           <div className="monthsWrapper__months" id="monthsWrapper__months">
             {calendarData.months.map(({ name }, index) => {
               const lastMonth = index === calendarData.months.length - 1;
@@ -104,6 +136,27 @@ const CalendarComponent = ({
               );
             })}
           </div>
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            color: "red",
+            right: "90px",
+            transform: "translateX(5px) translateY(-5px)",
+          }}
+          className="monthsWrapper__buttonWrapper"
+          onClick={() => jumpWithTheButton("right")}
+        >
+          <svg
+            key={idUniqueV2()}
+            xmlns="http://www.w3.org/2000/svg"
+            width="35"
+            height="36"
+            fill="white"
+            viewBox="0 0 16 16"
+          >
+            <path d={iconsPath.arrowRight} />
+          </svg>
         </div>
       </div>
 
