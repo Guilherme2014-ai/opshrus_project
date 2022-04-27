c5ce7ebe8b46a4a686f94fc2832dab137d79b357
Guilherme2014-ai <guilhermehenrique78383@gmail.com>
2022-04-27 11:46:42 -0300

    topics with white theme

--- src/components/minor/TopicsScrollComponent.tsx
+++ src/components/minor/TopicsScrollComponent.tsx
@@ -3,18 +3,26 @@
 import React, { useEffect, useState } from "react";
 import { idUniqueV2 } from "id-unique-protocol";
 
+// Contexts
+import { getWhiteThemeState } from "../../contexts/whiteThemeContext";
+
 // Interfaces
 import { topics } from "../../mockedData/topics";
 
 // Defaults
+import { defaultsTransition } from "../../defaults/pre-styles";
 import iconsPath from "../../defaults/iconsPath";
+import colors from "../../defaults/colors";
 
 // CSS
 import "./style/TopicsScrollComponent.scss";
 
-// Responsividade faltando
-
 const TopicsScrollComponent = () => {
+  const [whiteTheme, setWhiteTheme] = getWhiteThemeState() as [
+    boolean,
+    React.Dispatch<boolean>,
+  ];
+
   useEffect(() => {
     const topicsScroll__intermediateElement = document.getElementById(
       "topicsScroll__intermediate",
@@ -44,13 +52,29 @@ const TopicsScrollComponent = () => {
 
   return (
     <div className="topicsScroll" id="topicsScroll">
-      <div className="topicsScroll--horizontalGradient"></div>
+      <div
+        className="topicsScroll--horizontalGradient"
+        style={{
+          background: whiteTheme
+            ? `linear-gradient(to left, ${colors.primaryColorWhiteTheme} 0.5%, transparent 20%)`
+            : `linear-gradient(to left, ${colors.primaryColor} 0.5%, transparent 20%)`,
+        }}
+      ></div>
       <div
         className="topicsScroll__intermediate"
         id="topicsScroll__intermediate"
       >
         {topics.map((topic) => (
-          <div className="topicsScroll__intermediate__topic" key={idUniqueV2()}>
+          <div
+            className="topicsScroll__intermediate__topic"
+            key={idUniqueV2()}
+            style={{
+              boxShadow: whiteTheme ? "rgb(189, 189, 189) 4px 8px 30px" : "",
+              background: whiteTheme
+                ? `linear-gradient(324deg, ${colors.primaryColorWhiteTheme} 0%,#715AFF 150%)`
+                : "linear-gradient(324deg, rgba(48,52,63,1) 0%, #E5E5E5 150%)",
+            }}
+          >
             <strong>{topic.name}</strong>
           </div>
         ))}
